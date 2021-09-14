3ae473d37907eca79c3e3a50b780b6559ce72b1a
joshua2421 <josh.ramirez@duocuc.cl>
2021-09-14 16:31:16 -0300

    11

--- src/app/ingresar/ingresar.page.ts
+++ src/app/ingresar/ingresar.page.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingresar.page.scss'],
 })
 export class IngresarPage implements OnInit {
-
+  nombre_v: String;
+  contra_v : String;
   constructor() { }
 
   ngOnInit() {
   }
-
+  Datos_alumno(nombre: HTMLInputElement ,contrasena:HTMLInputElement)
+  {
+    this.nombre_v= "Joshua123"
+    this.contra_v = "1234"
+    let nom = nombre;
+    let contra = contrasena;
+  }
 }
